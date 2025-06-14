e86fb1af77c1cc7e9138dc10725c1755c841ac19
Anuvind <anuvindm02@gmail,com>
2025-06-14 21:42:36 +0530

    Pagination for messages

--- ChatServer.API/Hubs/ChatHub.cs
+++ ChatServer.API/Hubs/ChatHub.cs
@@ -4,30 +4,36 @@ using ChatServer.Application.Interfaces;
 using ChatServer.Application.Interfaces.Repositories;
 using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
+using ChatServer.Domain.Entities;
 
 namespace ChatServer.API.Hubs
 {
     [Authorize]
-    public class ChatHub: Hub
+    public class ChatHub : Hub
     {
         private readonly IChatRepository _chatService;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<Conversation> _conversationRepository;
         public ChatHub(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
             _chatService = _unitOfWork.CustomRepository<IChatRepository>();
+            _conversationRepository = _unitOfWork.GetRepository<Conversation>();
         }
 
         public async Task SendPrivateMessage(ChatMessageDto message)
         {
-                var savedMessage = await _chatService.SaveMessageAsync(message);
-                await _unitOfWork.SaveChangesAsync();
+            var savedMessage = await _chatService.SaveMessageAsync(message);
+            var conversation = await _conversationRepository.GetByIdAsync(message.ConversationId)
+                ??throw new KeyNotFoundException($"Conversation not found ${message.ConversationId}");
+            conversation.UpdatedAt = DateTimeOffset.UtcNow;
+            await _unitOfWork.SaveChangesAsync();
 
-                var participantIds = await _chatService.GetParticipantUserIdsAsync(message.ConversationId);
-                foreach (var participantId in participantIds.Where(id => id != message.SenderId))
-                {
-                    await Clients.User(participantId.ToString()).SendAsync("ReceiveMessage", Context.UserIdentifier, savedMessage);
-                }
+            var participantIds = await _chatService.GetParticipantUserIdsAsync(message.ConversationId);
+            foreach (var participantId in participantIds.Where(id => id != message.SenderId))
+            {
+                await Clients.User(participantId.ToString()).SendAsync("ReceiveMessage", Context.UserIdentifier, savedMessage);
+            }
         }
 
         public async Task SendGroupMessage(ChatMessageDto message, CancellationToken cancellationToken)
--- ChatServer.Application/Interfaces/IChatRepository.cs
+++ ChatServer.Application/Interfaces/IChatRepository.cs
@@ -6,7 +6,7 @@ namespace ChatServer.Application.Interfaces
     public interface IChatRepository
     {
         Task<ChatMessageDto> SaveMessageAsync(ChatMessageDto message);
-        Task<IEnumerable<ChatMessageDto>> GetConversationHistoryAsync(Guid conversationId, int skip = 0, int take = 50);
+        Task<IEnumerable<ChatMessageDto>> GetConversationHistoryAsync(Guid conversationId, DateTimeOffset? cursor, int limit);
         Task<Guid> CreateP2PConversationAsync(int user1Id, int user2Id, CancellationToken cancellationToken);
         Task<Conversation> CreateGroupConversationAsync(string groupName, IEnumerable<int> userIds);
         Task AddUserToGroupAsync(Guid conversationId, int userId);
